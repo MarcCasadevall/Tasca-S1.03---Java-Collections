13ca415323b7bd36b017a41a9dce198597171d60
MarcCasadevall <apple5.92@hotmail.com>
2026-01-29 13:51:39 +0100

    feat: end task 3

--- src/main/java/Lvl1/Ex3/CapitalGame.java
+++ src/main/java/Lvl1/Ex3/CapitalGame.java
@@ -1,13 +1,12 @@
 package Lvl1.Ex3;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class CapitalGame {
-
-    public static HashMap<String, String> map() {
+    public static HashMap<String, String> createMap() {
         HashMap<String, String> countries = new HashMap<>();
 
         String route = "C:/Users/Marc/Desktop/Archivos It/countries.txt";
@@ -18,7 +17,7 @@ public class CapitalGame {
             reader = new BufferedReader(fileReader);
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] countryCapital = line.split("\\s+",2);
+                String[] countryCapital = line.split("\\s+", 2);
                 if (!line.trim().isEmpty()) {
                     countries.put(countryCapital[0].trim(), countryCapital[1].trim());
                 }
@@ -36,5 +35,56 @@ public class CapitalGame {
             }
         }
         return countries;
+    }
+
+    public static int game(HashMap<String,String> countries, String user) {
+
+        ArrayList<String> gameCountries = new ArrayList<>(countries.keySet());
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Okey " + user + "\nYou have to name the capitals of the 10 countries I'm about to list, let's start...");
+        int score = 0;
+
+        for (int i = 0; i < 10; i++) {
+
+            String country = gameCountries.get((int) (Math.random() * (gameCountries.size())));
+            System.out.println(country);
+            String answer = sc.nextLine().trim();
+            String capital = countries.get(country);
+
+            if (answer.equalsIgnoreCase(capital)) {
+                score++;
+                System.out.println("Correct " + score + " points.");
+            } else {
+                System.out.println("Wrong the capital is " + capital);
+            }
+
+            gameCountries.remove(country);
+
+        }
+        return score;
+    }
+
+    public static void saveScore(String user, int score) {
+        BufferedWriter bw = null;
+
+        try {
+            FileWriter fw = new FileWriter("C:\\Users\\Marc\\Desktop\\Archivos It\\users.txt", true);
+            bw = new BufferedWriter(fw);
+            bw.write(user + " " + score);
+            bw.newLine();
+        } catch (IOException e) {
+            System.out.println("Error,user.txt not found " +e.getMessage());
+        } finally {
+            if (bw != null) {
+                try{
+                    bw.close();
+                } catch (IOException e) {
+                    System.out.println("Error closing BufferedWriter " + e.getMessage());
+                }
+
+            }
+        }
+
     }
 }
--- src/main/java/Lvl1/Ex3/Main.java
+++ src/main/java/Lvl1/Ex3/Main.java
@@ -1,11 +1,17 @@
 package Lvl1.Ex3;
 
 import java.util.HashMap;
-import static Lvl1.Ex3.CapitalGame.map;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap<String, String> countries = map();
-        System.out.println(map());
+
+        Scanner sc = new Scanner(System.in);
+        HashMap<String, String> countries = CapitalGame.createMap();
+        System.out.println("Game start\n" + "Insert user name:");
+        String user = sc.nextLine();
+        int score = CapitalGame.game(countries,user);
+        CapitalGame.saveScore(user,score);
+        sc.close();
     }
 }
